cf758702d6427f4a92ab6eb8ec415c4f8542b015
Paweł Raszuk <pawel.raszuk@gmail.com>
2017-09-03 19:51:18 +0200

    Reset with ending orange boost

--- Coords.py
+++ Coords.py
@@ -6,6 +6,7 @@ class Cord:
     yes = (600, 600)            # Yes Button to confirmed reset
 
     pistol = (1270, 300)
-    abili = (1270, 400)
+    abili = (1270, 400)         # Ability button
+    abili1 = (1040, 480)        # First ability
     start = (1200, 415)         # Start new Timeline
     game = (700, 400)           # Just random place in window
--- Main.py
+++ Main.py
@@ -5,9 +5,10 @@ import numpy
 from PIL import ImageOps
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
-
+from datetime import datetime
 """
-app 1280x768 windowed moved to blocking by side and upper panel (start position on ubuntu 16.04)
+app 1280x768 windowed moved to blocking by side and upper panel
+(start pos on ubuntu 16.04)
 first pixel of game(65,52)
 last pixel of game(1344,819)
 
@@ -15,60 +16,75 @@ last pixel of game(1344,819)
 m = PyMouse()
 k = PyKeyboard()
 
+counter = 0
+
 
 def main():
-    global counter
-    counter = 150
     timer = time.time()
-    while counter < 250:
-        time_to_warp = checkTimeWarp()
-        if time_to_warp:
+    while True:
+        if checkTimeWarp() and checkIfOrange() is False:
             timeWarp()
-            counter += 1
         else:  # Wait until time warp will be available
-            if time.time() - timer > 30:
+            if time.time() - timer > 5:
                 timer = time.time()
-                buying()
+                buying(False)
             else:
                 time.sleep(1)
 
 
 def checkTimeWarp():
-    image = ImageOps.grayscale(grab_screen(Cord.tw[0], Cord.tw[1], Cord.tw[2], Cord.tw[3]))
+    image = ImageOps.grayscale(grab_screen(
+        Cord.tw[0], Cord.tw[1], Cord.tw[2], Cord.tw[3]))
     a = numpy.array(image.getcolors())
     a = a.sum()
     if 30000 <= a <= 32500:
-        print('Sum grayscale of Time Warp button: ', a)
+        # print('Sum grayscale of Time Warp button: ', a)
         return True
     else:
         return False
 
 
-def buying():
-    print('Buying team members')
-    keys = ('A', 'S', 'D', 'F', 'G')
+def checkIfOrange():
+    image = ImageOps.grayscale(grab_screen(
+        Cord.abili1[0], Cord.abili1[1], Cord.abili1[0]+1, Cord.abili1[1]+1))
+    a = numpy.array(image.getcolors())
+    a = a.sum()
+    if a < 170:
+        return False
+    else:
+        return True
+
+
+def buying(first):
+    # print('Buying team members')
+    m.click(Cord.game[0], Cord.game[1])
+    keys = ('G', 'F', 'D', 'S', 'A')
+    if first:
+        # After clean start keys must be in normal order to unlock guns
+        keys = reversed(keys)
     for key in keys:
         k.tap_key(key)
         time.sleep(.1)
 
 
 def timeWarp():
-    print('TimeWarping')
+    print(datetime.now(), '   > TimeWarping #', counter)
+    global counter
+    counter += 1
     m.click(Cord.tw1[0], Cord.tw1[1])  # Time warp Button
     time.sleep(1)
-    print('Confirming')
+    # print('Confirming')
     m.click(Cord.yes[0], Cord.yes[1])  # Confiming Time warp action
-    print('Remaining: ', 250 - counter)
     time.sleep(2)
-    print('Skipping artefacts')
+    # print('Skipping artefacts')
     m.click(Cord.start[0], Cord.start[1])
-    print('CleanStart')
+    # print('CleanStart')
     time.sleep(6)
     cleanStart()
 
 
 def cleanStart():
-    print('Buying abilities and pistol upgrades')
+    # print('Buying abilities and pistol upgrades')
     for i in range(13):
         m.click(Cord.pistol[0], Cord.pistol[1])
         time.sleep(.1)
@@ -84,13 +100,14 @@ def cleanStart():
     k.tap_key('0')
     time.sleep(.1)
     # Hire each team member
-    buying()
+    buying(True)
     # Set Idle Mode to true
     keys = ('Q', 'W', 'E')
     for key in keys:
         time.sleep(.1)
         k.tap_key(key)
-    time.sleep(2)   # To prevent auto fake time warping cause delay of changing graphic.
+    # To prevent auto fake time warping cause delay of changing graphic.
+    time.sleep(2)
 
 
 main()
