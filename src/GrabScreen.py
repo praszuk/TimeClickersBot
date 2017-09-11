a6c421964e3d62193ce5d309e198495c7b5de87e
Paweł Raszuk <pawel.raszuk@gmail.com>
2017-09-12 00:00:00 +0200

    Added pyautogui (cross-platform) to grabbing screen

--- src/GrabScreen.py
+++ src/GrabScreen.py
@@ -1,11 +1,29 @@
 import ctypes
+import platform
 from PIL import Image
+from pyautogui import screenshot
 
-path = "C/prtscn.so"
+
+path = 'C/prtscn.so'
 grab = ctypes.CDLL(path)
 
 
 def grab_screen(x1, y1, x2, y2):
+    if platform.system() == 'Linux':
+        return grab_screen_linux(x1, y1, x2, y2)
+    else:
+        return screenshot(region=(x1, y1, x2, y2))
+
+
+"""
+It's function only for linux
+Compiled from C it has better performance than pyautogui.
+- From doc. pyautogui has ~10FPS with 1920x1080
+- From testing program in C has ~20 FPS with 1920x1080
+"""
+
+
+def grab_screen_linux(x1, y1, x2, y2):
     w, h = x2-x1, y2-y1
     size = w * h
     objlength = size * 3
